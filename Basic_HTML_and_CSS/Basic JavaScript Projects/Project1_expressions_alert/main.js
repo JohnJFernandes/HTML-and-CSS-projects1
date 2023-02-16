8a48475015e4b78b06755ac5f8b6b2a2451f6fc3
Zach Fernandez <121633815+JohnJFernandes@users.noreply.github.com>
2023-02-15 20:48:43 -0800

    pg 69 JavaScript assingment
    pg 69 JavaScript assingment

--- Basic_HTML_and_CSS/Basic JavaScript Projects/Project1_expressions_alert/main.js
+++ Basic_HTML_and_CSS/Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -1,3 +1,9 @@
 
 document.getElementById(apples);
 document.getElementById(oranges);
+
+function myFunction ()  {                     //button display when clicked//
+    var sentence = "I am learning, slowly\n";
+    sentence += "a little from this book!";
+    document.getElementById("Concatenate").innerHTML =sentence;
+}
