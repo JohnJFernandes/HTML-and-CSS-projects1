3bb2460ae7536facb0def4cc2e13b1e0225abf69
Zach Fernandez <121633815+JohnJFernandes@users.noreply.github.com>
2023-02-15 20:27:04 -0800

    requested files for pg64 JavaScript assignment

--- Basic_HTML_and_CSS/Basic JavaScript Projects/Project1_expressions_alert/main.js
+++ Basic_HTML_and_CSS/Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -1,26 +1,3 @@
-console.log("Hello World & the universe");
 
-window.alert("Everyone has to wake")
-
-document.write('"HELLO WORLD"');
-
-document.write('Everyone has to wake up_'
-+ 'the oceans are dying, don\'t you like fresh seafood?_'
-+ 'We need to save the seas if we want to eat.\n');
-
-var A = "Please donate to save the shrimps campaign"
-document.write (A)
-
-var family = "Seafood", Shrimps="You got prime rib", Salmon="BBQ or Smoked",
-    Oysters ="Most Iron Nutrient food", Crab = "Cakes or with butter";
-document.write(Shrimps);
-
-document.write(Name = "what the person types in the name box")
-
-function My_First_Function () {                                                 //created my first function, defined and named
-        var str = "Click here to read a great article on food sustainability";  //defining my variable and giving it a string value
-        document.getElementById=("Button_Text").innerHTML = str;
-}
-var Sent1 = "This is the beginnning of the string\n"
-var Sent2 = "and this is the end of the string"
-document.write(Sent1 + Sent2)
+document.getElementById(apples);
+document.getElementById(oranges);
